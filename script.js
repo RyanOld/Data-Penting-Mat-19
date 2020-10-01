dea3005dfc0e5fcc192d3da0de78287654d54179
Aziz Nurhidayat <aziznurhidayata@gmail.com>
2020-10-01 08:35:35 +0700

    massive changes. height animation working smoothly

--- script.js
+++ script.js
@@ -1,42 +1,28 @@
 //Visual Coding
-let subjectTitle = document.querySelectorAll(".subject-title a");
-let subjectDetail = document.querySelectorAll(".subject-detail");
-// let meetNo = document.querySelectorAll(".meet");
+const subjectTitle = document.querySelectorAll(".subject-title a");
+const subjectDetail = document.querySelectorAll(".subject-detail .wrapper");
 
-function reveal1 (e) {
+const heights = document.querySelectorAll(".subject-detail");
+document.documentElement.style
+  .setProperty('--probab', `${heights[0].offsetHeight+14}px`);
+document.documentElement.style
+  .setProperty('--geoa', `${heights[1].offsetHeight+14}px`);
+document.documentElement.style
+  .setProperty('--prokom', `${heights[2].offsetHeight+14}px`);
+document.documentElement.style
+  .setProperty('--kalut', `${heights[3].offsetHeight+14}px`);
+
+subjectTitle.forEach((element, index) => {
+  element.addEventListener("click", (e) => {
   e.preventDefault();
-  subjectDetail[0].classList.toggle("hidden");
-}
-function reveal2(e) {
-  e.preventDefault();
-  subjectDetail[1].classList.toggle("hidden");
-}
-function reveal3(e) {
-  e.preventDefault();
-  subjectDetail[2].classList.toggle("hidden");
-}
-function reveal4(e) {
-  e.preventDefault();
-  subjectDetail[3].classList.toggle("hidden");
-}
-
-// function reveal15 (e) {
-//   e.preventDefault();
-//   meetNo[0].classList.toggle("hidden");
-// }
-
-
-subjectTitle[0].addEventListener("click", reveal1, false);
-subjectTitle[1].addEventListener("click", reveal2, false);
-subjectTitle[3].addEventListener("click", reveal4, false);
-subjectTitle[2].addEventListener("click", reveal3, false);
-
-
-// function reveal (e, index) {
-//   // e.preventDefault();
-//   subjectTitle[index].classList.toggle("hidden");
-// }
-
-// for (let index = 0; index < subjectTitle.length; index++) {
-//   subjectMeetNo[index].addEventListener("click", () => { }, false);
-// }
+  if (
+      !subjectDetail[index].classList.contains(`expanded${index+1}`) &&
+      !subjectDetail[index].classList.contains(`minimized${index+1}`)
+  ) {
+      subjectDetail[index].classList.add(`minimized${index+1}`);
+    } else {
+      subjectDetail[index].classList.toggle(`minimized${index+1}`);
+      subjectDetail[index].classList.toggle(`expanded${index+1}`);
+    }
+  }, false)
+})
