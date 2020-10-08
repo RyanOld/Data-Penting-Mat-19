81a97713607dccc939ad113790ef75bbf0fe9076
Aziz Nurhidayat <aziznurhidayata@gmail.com>
2020-10-08 22:27:25 +0700

    updated for mid-week 5

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-//Visual Coding
+//Visual Coding : animated hide-expand
 const subjectTitle = document.querySelectorAll(".subject-title a");
 const subjectDetail = document.querySelectorAll(".subject-detail .wrapper");
-
+  //height hack becoz percentage for the keyframes 
+  //doesn't work
 const heights = document.querySelectorAll(".subject-detail");
 document.documentElement.style
   .setProperty('--probab', `${heights[0].offsetHeight+14}px`);
@@ -11,7 +12,8 @@ document.documentElement.style
   .setProperty('--prokom', `${heights[2].offsetHeight+14}px`);
 document.documentElement.style
   .setProperty('--kalut', `${heights[3].offsetHeight+14}px`);
-
+  //functions got complicated because the stupid
+  //percentage value doesn't work.
 subjectTitle.forEach((element, index) => {
   element.addEventListener("click", (e) => {
   e.preventDefault();
